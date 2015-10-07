3a879c9389cd15bcecd2f8a8c59d48122c7db4aa
littleqz <qizhihere@gmail.com>
2015-10-07 23:56:04 +0800

    shot screen to different directories

--- lisp/init-app.lisp
+++ lisp/init-app.lisp
@@ -8,23 +8,30 @@
    (:string "Window Class to match: "))
   (run-or-raise command `(:class ,class)))
 
-(defvar *screenshot-dir* "~/Pictures/screenshots/")
-(defvar *screenshot-ext* ".png")
-(defcommand scrot (flags)
-    ((:rest "scrot flags: "))
-  (ensure-directories-exist *screenshot-dir*)
-  (let* ((dir (string-right-trim "/" *screenshot-dir*))
-         (fpath-gen (lambda () (concat *screenshot-dir* "/" (random-string) *screenshot-ext*)))
+(defcommand scrot (dir ext flags)
+  ((:string "save directory: ")
+   (:string "image extension: ")
+   (:rest "scrot flags: "))
+  (ensure-directories-exist dir)
+  (let* ((dir (string-right-trim "/" dir))
+         (fpath-gen (lambda () (concat dir "/" (random-string) ext)))
          (post-commands '("notify-send -i dialog-information -a scrot -t 5000 截图已保存至 \$f"
                           "echo \$f | xsel -bi"))
          (flags (concat " " flags " -e '" (reduce (lambda (a b) (concat a ";" b)) post-commands) "'"))
-         (fpath (funcall fpath-gen))
-         (retry 100))
-    (loop for i from 1 to retry
+         (fpath (funcall fpath-gen)))
+    (loop for i from 1 to 100
           until (not (probe-file fpath))
           do
           (setf fpath (funcall fpath-gen)))
     (run-shell-command (concat "scrot " fpath flags))))
+
+(defcommand scrot-to-default-directory (flags)
+  ((:rest "scrot flags: "))
+  (run-commands (concat "scrot ~/Pictures/screenshots .png " flags)))
+
+(defcommand scrot-to-blog-directory (flags)
+  ((:rest "scrot flags: "))
+  (run-commands (concat "scrot ~/org/img .png " flags)))
 
 (defun random-string (&optional (length 10))
   "Random string generator, default length is 10."
@@ -34,6 +41,10 @@
 (defvar *launch-app-map* (make-sparse-keymap))
 (map-keys *root-map* '(("c" "goto-app chromium Chromium")
                        ("C"        "run-shell-command chromium")
+                       ("Print"    "scrot-to-blog-directory \"\"") ;; empty placeholder
+                       ("C-Print"  "scrot-to-blog-directory -u")
+                       ("Sys_Req"  "scrot-to-blog-directory -s")
+                       ("M-Print"  "scrot-to-blog-directory -cd 5")
                        ("g" *launch-app-map*)))
 
 (map-keys *top-map* '(("s-l"      "run-shell-command ~/scripts/i3lock.sh")
@@ -43,10 +54,10 @@
                       ("C-s-RET"  "run-shell-command termite")
                       ("s-F1"     "run-shell-command xfce4-popup-whiskermenu")
                       ("s-F3"     "run-shell-command xfce4-popup-whiskermenu")
-                      ("Print"    "scrot \"\"") ;; empty placeholder
-                      ("C-Print"  "scrot -u")
-                      ("Sys_Req"  "scrot -s")
-                      ("M-Print"  "scrot -cd 5")
+                      ("Print"    "scrot-to-default-directory \"\"") ;; empty placeholder
+                      ("C-Print"  "scrot-to-default-directory -u")
+                      ("Sys_Req"  "scrot-to-default-directory -s")
+                      ("M-Print"  "scrot-to-default-directory -cd 5")
                       ("C-M-S"  "run-shell-command gpick -p")))
 
 
